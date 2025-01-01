5bd496399ca1844b83758d6aa73d1e363ccc3017
Jürgen Metzler <juergen.metzler@gmx.ch>
2025-01-01 11:09:19 +0100

    Added first draft of Pi4j implementation

--- Raspberry Pi image/Java/BeeCounterDaemon/src/main/java/com/apis_mellifera/BeeCounterApp.java
+++ Raspberry Pi image/Java/BeeCounterDaemon/src/main/java/com/apis_mellifera/BeeCounterApp.java
@@ -1,55 +1,68 @@
-/* Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.*/
 package com.apis_mellifera;
 
 
-import com.apis_mellifera.model.entity.BeeTraffic;
-import com.apis_mellifera.model.entity.HiveEntrance;
+import com.apis_mellifera.businessservice.BusinessServiceImpl;
 import com.apis_mellifera.model.entity.LightBarrier;
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.*;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Properties;
 
-/**
- * Maven archetype for generating a JPA application.
- * The sample connects to Oracle Database and does some database operations.
- * Make sure to use the schema (JDBCSampleData.sql) at https://tinyurl.com/y2a7x65z
- * to create the tables "emp" and "dept" required for this sample. 
- * @author Nirmala Sundarappa
- */
 public class BeeCounterApp {
-	public static void main(String[] args) {
-		//PersistenceProvider provider = new PersistenceProvider();
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("beecounterPU");
-		EntityManager manager = factory.createEntityManager();
+    public static void main(String[] args) {
+        //PersistenceProvider provider = new PersistenceProvider();
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("beecounterPU");
+        EntityManager entityManager = factory.createEntityManager();
 
-		EntityTransaction tx = manager.getTransaction();
-		tx.begin();
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
 
-		TypedQuery<HiveEntrance> hiveEntranceQuery = manager.createNamedQuery("HiveEntrance.findAll", HiveEntrance.class);
-		List<HiveEntrance> hiveEntrances = hiveEntranceQuery.getResultList();
-		System.out.println(String.format("%6s   %18s  %15s","ID", "Hive Entrance Name", "Number"));
-		System.out.println(String.format("%6s   %18s  %15s","------", "------------------", "-----------"));
-		for (HiveEntrance hiveEntrance : hiveEntrances) {
-			System.out.println(String.format("%6s   %18s  %15s",hiveEntrance.getId(), hiveEntrance.getName(), hiveEntrance.getNumber()));
-		}
+        // Retrieve all Light Barriers
+        BusinessServiceImpl businessService = new BusinessServiceImpl();
+        List<LightBarrier> lightBarriers = businessService.getAllLightBarriers(entityManager);
 
-		System.out.println("\nPrinting the list of Light barriers:");
-		TypedQuery<LightBarrier> lightBarrierQuery = manager.createNamedQuery("LightBarrier.findByHeId", LightBarrier.class).setParameter("heId", "1");
-		List<LightBarrier> lightBarriers = lightBarrierQuery.getResultList();
-		System.out.println(String.format("%15s   %15s  %15s  %15s","Hive Entrance ID", "Light Barrier ID", "Type", "GPIO Port"));
-		System.out.println(String.format("%15s   %15s  %15s  %15s","------", "-----------", "-----------", "-----------"));
-		for (LightBarrier lightBarrier : lightBarriers) {
-			System.out.println(String.format("%15s   %15s  %15s  %15s",lightBarrier.getHeId(), lightBarrier.getId(), lightBarrier.getType(), lightBarrier.getGpioPort() ));
-		}
+        initializeGpioPortforLightBarriers(lightBarriers);
 
-		System.out.println("\nPrinting the list of BeeTraffic:");
-		TypedQuery<BeeTraffic> beeTraficQuery = manager.createNamedQuery("BeeTraffic.findAll", BeeTraffic.class);
-		List<BeeTraffic> beeTrafficList = beeTraficQuery.getResultList();
-		System.out.println(String.format("%15s   %15s  %15s","Hive Entrance ID", "Light Barrier ID", "Type"));
-		System.out.println(String.format("%15s   %15s  %15s","------", "-----------", "-----------"));
-		for (BeeTraffic beeTraffic : beeTrafficList) {
-			System.out.println(String.format("%15s   %15s  %15s",beeTraffic.getId(), beeTraffic.getLbId(), beeTraffic.getLightBarrierCrossDateTime()));
-		}
-		tx.rollback();
-	}
+        tx.rollback();
+    }
+
+    private static void initializeGpioPortforLightBarriers(List<LightBarrier> lightBarriers) {
+        // Initialize Pi4J with an auto context
+        // An auto context includes AUTO-DETECT BINDINGS enabled
+        // which will load all detected Pi4J extension libraries
+        // (Platforms and Providers) in the class path
+        Context pi4j = Pi4J.newAutoContext();
+        System.setProperty("pi4j.host", "10.0.0.50");
+        // get a Digital Input I/O provider from the Pi4J context
+        DigitalInputProvider digitalInputProvider = pi4j.provider("pigpio-digital-input");
+
+        for (LightBarrier lightBarrier : lightBarriers) {
+
+            System.out.println("Initializing digital input for ".concat(lightBarrier.getType()).concat(" Light Barrier (ID: ".concat(lightBarrier.getId().toString())).concat(") on GPIO port ").concat(lightBarrier.getGpioPort().toString()));
+            Properties properties = new Properties();
+            properties.put("id", lightBarrier.getId().toString());
+            properties.put("address", lightBarrier.getGpioPort());
+            properties.put("pull", "UP");
+            properties.put("name", lightBarrier.getType().concat(" Light Barrier (ID: ".concat(lightBarrier.getId().toString()).concat(")")));
+
+            DigitalInputConfig config = DigitalInput.newConfigBuilder(pi4j)
+                    .load(properties)
+                    .build();
+
+            DigitalInput input = pi4j.din().create(config);
+            input.addListener(new DigitalStateChangeListener() {
+                @Override
+                public void onDigitalStateChange(DigitalStateChangeEvent event) {
+
+                    System.out.println("DIGITAL INPUT [");
+                    System.out.println(event.source());
+                    System.out.println("] STATE CHANGE: ");
+                    //System.out.println(event.state());
+                }
+            });
+        }
+    }
 }
