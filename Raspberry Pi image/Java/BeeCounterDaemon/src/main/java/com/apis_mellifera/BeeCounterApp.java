70e525b8f4c1269f38a4b44f4d2c45b94ed3b47c
Jürgen Metzler <juergen.metzler@gmx.ch>
2025-01-02 23:23:40 +0100

    PullResistance UP configuration

--- Raspberry Pi image/Java/BeeCounterDaemon/src/main/java/com/apis_mellifera/BeeCounterApp.java
+++ Raspberry Pi image/Java/BeeCounterDaemon/src/main/java/com/apis_mellifera/BeeCounterApp.java
@@ -86,7 +86,7 @@ public class BeeCounterApp {
                     .id(lightBarrier.getId().toString())
                     .name(lightBarrier.getType().concat(" Light Barrier (ID: ").concat(lightBarrier.getId().toString()))
                     .address(lightBarrier.getGpioPin())
-                    .pull(PullResistance.UP)
+                    .pull(PullResistance.PULL_UP)
                     .build();
 
             var lightBarrierInput = pi4jContext.create(lightBarrierConfig);
