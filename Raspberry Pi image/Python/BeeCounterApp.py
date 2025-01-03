72a19ddeafdde616a4984686880e09cd6a949145
Jürgen Metzler <juergen.metzler@gmx.ch>
2025-01-03 18:00:33 +0100

    Fixed formatting

--- Raspberry Pi image/Python/BeeCounterApp.py
+++ Raspberry Pi image/Python/BeeCounterApp.py
@@ -42,7 +42,7 @@ def insert_event_into_db():
         print("Monitoring GPIO input ...")
         while True:
             pass
-     except KeyboardInterrupt:
+    except KeyboardInterrupt:
         print ("Exiting...")
-     finally:
+    finally:
         GPIO.cleanup()
