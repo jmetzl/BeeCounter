7aeb72f92dbf3a8502c6b71ae333a6ca2a99b463
Jürgen Metzler <juergen.metzler@gmx.ch>
2025-03-16 09:54:47 +0100

    Updated pins/GPIO

--- Raspberry Pi image/Python/BeeCounter.py
+++ Raspberry Pi image/Python/BeeCounter.py
@@ -8,52 +8,52 @@ import time
 
 # ----------------------------------
 # Entrance 1
-# Inner Pin 11/GPIO 17
-entrance_1_inner_light_barrier = 17
-# Outer Pin 12/GPIO 18
-entrance_1_outer_light_barrier = 18
+# Inner Pin 11/GPIO 0
+entrance_1_inner_light_barrier = 0
+# Outer Pin 12/GPIO 1
+entrance_1_outer_light_barrier = 1
 
 # ----------------------------------
 # Entrance 2
-# Inner Pin 13/GPIO 27
-entrance_2_inner_light_barrier = 27
-# Outer Pin 15/GPIO 22
-entrance_2_outer_light_barrier = 22
+# Inner Pin 13/GPIO 2
+entrance_2_inner_light_barrier = 2
+# Outer Pin 15/GPIO 3
+entrance_2_outer_light_barrier = 3
 
 # ----------------------------------
 # Entrance 3
-# Inner Pin 16/GPIO 23
+# Inner Pin 16/GPIO 23 ?
 entrance_3_inner_light_barrier = 23
-# Outer Pin 18/GPIO 24
-entrance_3_outer_light_barrier = 24
+# Outer Pin 18/GPIO 5
+entrance_3_outer_light_barrier = 5
 
 # ----------------------------------
 # Entrance 4
-# Inner Pin 22/GPIO 25
-entrance_4_inner_light_barrier = 25
-# Outer Pin 7/GPIO 4
-entrance_4_outer_light_barrier = 4
+# Inner Pin 22/GPIO 6
+entrance_4_inner_light_barrier = 6
+# Outer Pin 7/GPIO 7
+entrance_4_outer_light_barrier = 7
 
 # ----------------------------------
 # Entrance 5
-# Inner Pin 8/GPIO 14
-entrance_5_inner_light_barrier = 14
-# Outer Pin 9/GND???
-entrance_5_outer_light_barrier = 4
+# Inner Pin 5/GPIO 9
+entrance_5_inner_light_barrier = 9
+# Outer Pin 10/GPIO 16
+entrance_5_outer_light_barrier = 16
 
 # ----------------------------------
 # Entrance 6
-# Inner Pin 24/GPIO 8
-entrance_6_inner_light_barrier = 8
-# Outer Pin 26/GPIO 7
-entrance_6_outer_light_barrier = 7
+# Inner Pin 24/GPIO 10
+entrance_6_inner_light_barrier = 10
+# Outer Pin 26/GPIO 11
+entrance_6_outer_light_barrier = 11
 
 # ----------------------------------
 # Entrance 7
-# Inner Pin 19/GPIO 10
-entrance_7_inner_light_barrier = 10
-# Outer Pin 21/GPIO 9
-entrance_7_outer_light_barrier = 9
+# Inner Pin 19/GPIO 12
+entrance_7_inner_light_barrier = 12
+# Outer Pin 21/GPIO 13
+entrance_7_outer_light_barrier = 13
 
 # Setup GPIO mode to Broadcom
 GPIO.setmode(GPIO.BCM)
